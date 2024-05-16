e251d8d73427cabb229f53b362f1b6055ff70f94
Jehoon <limjhoon8@gmail.com>
2024-05-16 11:23:27 +0900

    file update

--- src/main/java/com/study/springstudy/springmvc/chap01/BasicController.java
+++ src/main/java/com/study/springstudy/springmvc/chap01/BasicController.java
@@ -50,7 +50,7 @@ public class BasicController {
     }
 
     // 3. 커맨드 객체 (RequestDTO) 사용하기
-    // ex) /spring/chap01/order?orderNum=22&goods=구두&amount=3$price+2000...
+    // ex) /spring/chap01/order?orderNum=22&goods=구두&amount=3&price=2000...
     @RequestMapping("/order")
     public String order(OrderDto order) {
 
