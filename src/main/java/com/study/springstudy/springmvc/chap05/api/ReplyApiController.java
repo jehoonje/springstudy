16c993ed86ba3bf3f4b80bc281c9b603b87e3b66
Jehoon <limjhoon8@gmail.com>
2024-05-24 12:35:37 +0900

    3 tier architecture - part2

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -5,6 +5,7 @@ import com.study.springstudy.springmvc.chap05.dto.response.ReplyDetailDto;
 import com.study.springstudy.springmvc.chap05.entity.Reply;
 import com.study.springstudy.springmvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/replies")
 @RequiredArgsConstructor
+@Slf4j
 public class ReplyApiController {
     
     private final ReplyService replyService;
@@ -26,8 +28,24 @@ public class ReplyApiController {
     @GetMapping("/{bno}")
     public ResponseEntity<?> list(@PathVariable long bno) {
 
+        if (bno == 0) {
+            String message = "글 번호는 0번이 될 수 없습니다.";
+            log.warn("글 번호는 0번이 될 수 없습니다.");
+            return ResponseEntity
+                    .badRequest()
+                    .body(message);
+        }
+
+        log.info("/api/v1/replies/{} : GET", bno);
 
         List<ReplyDetailDto> replies = replyService.getReplies(bno);
+        log.debug("first reply : {}", replies.get(0));
+
+        try {
+
+        } catch (Exception e) {
+            log.error("fjsaie");
+        }
 
         return ResponseEntity
                 .ok()
