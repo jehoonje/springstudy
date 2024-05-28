54e499bfc48b8747f7fa629b7818948468db8be6
Jehoon <limjhoon8@gmail.com>
2024-05-28 16:46:02 +0900

    reply paging update

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -1,8 +1,9 @@
 package com.study.springstudy.springmvc.chap05.api;
 
+import com.study.springstudy.springmvc.chap04.common.Page;
 import com.study.springstudy.springmvc.chap05.dto.request.ReplyPostDto;
 import com.study.springstudy.springmvc.chap05.dto.response.ReplyDetailDto;
-import com.study.springstudy.springmvc.chap05.entity.Reply;
+import com.study.springstudy.springmvc.chap05.dto.response.ReplyListDto;
 import com.study.springstudy.springmvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,21 +17,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// 서버에 던지는 클래스
 @RestController
 @RequestMapping("/api/v1/replies")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin // CORS 정책 허용범위 설정
 public class ReplyApiController {
 
     private final ReplyService replyService;
 
     // 댓글 목록 조회 요청
-    // URL : /api/v1/replies/원본글번호   -  GET -> 목록조회
+    // URL : /api/v1/replies/원본글번호/page/페이지번호   -  GET -> 목록조회
     // @PathVariable : URL에 붙어있는 변수값을 읽는 아노테이션
-    @GetMapping("/{bno}")
-    public ResponseEntity<?> list(@PathVariable long bno) {
+    @GetMapping("/{bno}/page/{pageNo}")
+    public ResponseEntity<?> list(
+            @PathVariable long bno
+            , @PathVariable int pageNo
+    ) {
 
         if (bno == 0) {
             String message = "글 번호는 0번이 될 수 없습니다.";
@@ -42,8 +44,7 @@ public class ReplyApiController {
 
         log.info("/api/v1/replies/{} : GET", bno);
 
-        List<ReplyDetailDto> replies = replyService.getReplies(bno);
-        log.debug("first reply : {}", replies.get(0));
+        ReplyListDto replies = replyService.getReplies(bno, new Page(pageNo, 10));
 
         return ResponseEntity
                 .ok()
@@ -53,18 +54,16 @@ public class ReplyApiController {
     // 댓글 생성 요청
     // @RequestBody : 클라이언트가 전송한 데이터를 JSON으로 받아서 파싱
     @PostMapping
-    public ResponseEntity<?> posts(@Validated
-                                       @RequestBody
-                                       ReplyPostDto dto,
-                                   BindingResult result) {
-
+    public ResponseEntity<?> posts(
+            @Validated @RequestBody ReplyPostDto dto
+            , BindingResult result // 입력값 검증 결과 데이터를 갖고 있는 객체
+    ) {
         log.info("/api/v1/replies : POST");
         log.debug("parameter: {}", dto);
-        
+
         if (result.hasErrors()) {
-            
             Map<String, String> errors = makeValidationMessageMap(result);
-            
+
             return ResponseEntity
                     .badRequest()
                     .body(errors);
@@ -78,14 +77,14 @@ public class ReplyApiController {
 
         return ResponseEntity
                 .ok()
-                .body(replyService.getReplies(dto.getBno()));
+                .body(replyService.getReplies(dto.getBno(), new Page(1, 10)));
     }
 
     private Map<String, String> makeValidationMessageMap(BindingResult result) {
 
         Map<String, String> errors = new HashMap<>();
 
-        // 에러 정보가 들어있는 리스트
+        // 에러정보가 모여있는 리스트
         List<FieldError> fieldErrors = result.getFieldErrors();
 
         for (FieldError error : fieldErrors) {
@@ -99,10 +98,11 @@ public class ReplyApiController {
     @DeleteMapping("/{rno}")
     public ResponseEntity<?> delete(@PathVariable long rno) {
 
-        List<ReplyDetailDto> dtoList = replyService.remove(rno);
+        ReplyListDto dtoList = replyService.remove(rno);
 
         return ResponseEntity
                 .ok()
                 .body(dtoList);
     }
+
 }
